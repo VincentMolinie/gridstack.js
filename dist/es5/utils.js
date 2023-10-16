c34aa6b3fff60d4e6d34a604b233725183e66089
Vincent Molinié <vincent.m@forestadmin.com>
2023-10-16 11:25:05 +0200

    debug invalid height

--- dist/es5/utils.js
+++ dist/es5/utils.js
@@ -240,7 +240,7 @@ var Utils = /** @class */ (function () {
             else {
                 var match = val.match(/^(-[0-9]+\.[0-9]+|[0-9]*\.[0-9]+|-[0-9]+|[0-9]+)(px|em|rem|vh|vw|%)?$/);
                 if (!match) {
-                    throw new Error("Invalid height ".concat(val));
+                    throw new Error("Invalid height \"".concat(val, "\""));
                 }
                 unit = match[2] || 'px';
                 h = parseFloat(match[1]);
